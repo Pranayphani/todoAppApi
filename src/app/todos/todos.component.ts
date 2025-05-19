ab577fa1d6a1d2fd5e5901376013c6137630f499
pranaykumarsamala <samalapranay02@gmail.com>
2025-05-19 22:34:48 +0530

    api calls

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -14,34 +14,13 @@ import { FormsModule } from '@angular/forms';
 })
 export class TodosComponent implements OnInit{
   todoService=inject(TodosService);
-  todoItems=signal<Array<Todo>>([]);
-  searchTerm=signal('');
-
+  
+  todoItems: Todo[]=[];
   ngOnInit(){
     this.todoService.getTodosFromApi()
-    .pipe(
-      catchError((err)=>{
-        console.log(err);
-        throw err;
-      })
-    )
     .subscribe((todos)=>{
       console.log(todos);
-      this.todoItems.set(todos);
-    });
-  }
-
-  updateTodoItem(todoItem: Todo){
-    this.todoItems.update((todos)=>{
-      return todos.map(todo=>{
-        if(todo.id===todoItem.id){
-          return {
-            ...todo,
-            completed: !todo.completed
-          };
-        }
-        return todo;
-      });
+      this.todoItems=todos;
     });
   }
 }
